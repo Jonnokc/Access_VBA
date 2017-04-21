b48a213dd1bd3a585bdae959529aae6de62be9c6
Jonnokc <JA052464@cerner.net>
2017-04-21 10:10:31 -0500

    Confirm user wants to add new record

---
+++ SubmittedCodeDatabase/Confirm_Data_Entry.vb
@@ -0,0 +1,38 @@
+Private Sub Form_BeforeUpdate(Cancel As Integer)
+' perform data validation
+
+' Will need to change Control Field to match each use
+    If IsNull(Me.ClientMnemonicID) Then
+        MsgBox "You must enter a New Blacklisted Code System.", vbCritical, "Data entry error..."
+        DoCmd.GoToControl "ClientMnemonicID"
+
+        Cancel = True
+    End If
+    If Not Cancel Then
+        ' passed the validation process
+        If Me.NewRecord Then
+            If MsgBox("Data will be saved, Are you Sure?", vbYesNo, "Confirm") = vbNo Then
+                Cancel = True
+            Else
+                ' run code for new record before saving
+
+            End If
+
+        Else
+            If MsgBox("Data will be modified, Are you Sure?", vbYesNo, "Confirm") = vbNo Then
+                Cancel = True
+            Else
+                ' run code before an existing record is saved
+                ' example: update date last modified
+
+            End If
+        End If
+    End If
+    ' if the save has been canceled or did not pass the validation , then ask to Undo changes
+    If Cancel Then
+        If MsgBox("Do you want to undo all changes?", vbYesNo, "Confirm") = vbYes Then
+            Me.Undo
+        End If
+
+    End If
+End Sub
